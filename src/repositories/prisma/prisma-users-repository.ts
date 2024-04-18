1e26afeec68451a6af6dfc14bb7fb9ab543b8ce8
Gabriel Dias <gabrieldias7820@gmail.com>
2024-04-18 16:37:08 -0300

    interface do usuário

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,22 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+import { UserRepository } from '../users-repository'
+
+export class PrismaUsersRepository implements UserRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+    return user
+  }
+}
