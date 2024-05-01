982ee578df17b109b6766cea9cc6b3ce4cdce1d2
Gabriel Dias <gabrieldias7820@gmail.com>
2024-05-01 15:50:51 -0300

    caso de uso de histórico

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,29 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { CheckIn } from '@prisma/client'
+
+interface FetchUserCheckInsHistoryRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsHistoryResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistory {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryRequest): Promise<FetchUserCheckInsHistoryResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+
+    return {
+      checkIns,
+    }
+  }
+}
