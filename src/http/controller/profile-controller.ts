3f65973e3de0072c03798dab1c00d3ccfe20670f
Gabriel Dias <gabrieldias7820@gmail.com>
2024-05-04 18:59:41 -0300

    controller de perfil

--- src/http/controller/profile-controller.ts
+++ src/http/controller/profile-controller.ts
@@ -1,9 +1,19 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify()
 
-  console.log(request.user.sub)
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  return reply.status(200).send()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
