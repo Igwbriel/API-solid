c17ac6ab2a416ae8fcb2b05046d3ae0cec93ef7c
Gabriel Dias <gabrieldias7820@gmail.com>
2024-05-08 20:29:24 -0300

    controller de pesquisa de academias

---
+++ src/http/controller/gyms/search-controller.ts
@@ -0,0 +1,23 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gym-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsBodySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { query, page } = searchGymsBodySchema.parse(request.body)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+
+  const { gyms } = await searchGymsUseCase.execute({
+    query,
+    page,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
