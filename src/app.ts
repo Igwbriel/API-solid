0e05ad67696be54e77f7eb8bb530698785823226
Gabriel Dias <gabrieldias7820@gmail.com>
2024-05-11 12:36:20 -0300

    estratégia de refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import fastify from 'fastify'
-import { usersRoutes } from './http/controller/users/routes'
-import { ZodError } from 'zod'
-import { env } from './env'
 import { fastifyJwt } from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+import { env } from './env'
+import { ZodError } from 'zod'
+import { usersRoutes } from './http/controller/users/routes'
 import { gymsRoutes } from './http/controller/gyms/routes'
 import { checkInRoutes } from './http/controller/check-ins/routes'
 
@@ -10,7 +11,15 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
