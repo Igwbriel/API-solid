534b4921b6aceca9ffabc2b9c2afd8dcb7732758
Gabriel Dias <gabrieldias7820@gmail.com>
2024-05-12 21:07:07 -0300

    testes de RBAC

--- src/http/controller/check-ins/validate.spec.ts
+++ src/http/controller/check-ins/validate.spec.ts
@@ -14,7 +14,7 @@ describe('Validate checkIn (e2e)', () => {
   })
 
   it('should be able to validate a checkIn', async () => {
-    const { token } = await createAndAuthenticateUser(app)
+    const { token } = await createAndAuthenticateUser(app, true)
 
     const user = await prisma.user.findFirstOrThrow()
 
